561d135fc6c957f3ddc83383ac1c6b95764177ff
Hugo LIEGEARD <hugo@biyn.media>
2020-05-13 16:30:08 +0200

    Travail du 13/05
    Mise en Place d'une page Contact

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,15 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -86,6 +94,95 @@ class DefaultController extends AbstractController
         # Transmission au Menu
         return $this->render('components/_nav.html.twig', [
             'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Page Contact
+     * @Route("/contact.html", name="default_contact", methods={"POST|GET"})
+     */
+    public function contact(Request $request)
+    {
+        # Création d'une demande de contact
+        $contact = new Contact();
+
+        # Conception du formulaire
+        $form = $this->createFormBuilder($contact)
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre nom'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre description'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre email'
+                ]
+            ])
+            ->add('telephone', TelType::class, [
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre numéro de téléphone'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        #----------
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            # Insertion dans la BDD
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            # Notification
+            $this->addFlash('notice', 'Message envoye avec succes !');
+
+            # Redirection
+            return $this->redirectToRoute('default_contact');
+        }
+
+        return $this->render('default/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/gestion-contact.html", name="default_gestion",   methods={"GET"})          )
+     */
+    public function message()
+    {
+        # Récupération des messages
+        $messages = $this->getDoctrine()
+            ->getRepository(Contact::class)
+            ->findAll();
+
+        # Transmission à la vue
+        return  $this->render('default/gestion-contact.html.twig', [
+            'messages' => $messages
         ]);
     }
 }
